a34c1f9ccb58b1598b0492af5ac22192bfbdbfad
92257852915 <artur.santos@serpro.gov.br>
2021-11-25 10:58:31 -0300

    teste animal

---
+++ src/orientacao_objetos/Animais.java
@@ -0,0 +1,9 @@
+package orientacao_objetos;
+
+public class Animais {
+    String nome;
+
+    void respostaCachorro(){
+        System.out.println("au au");
+    }
+}
---
+++ src/orientacao_objetos/Principal.java
@@ -0,0 +1,10 @@
+package orientacao_objetos;
+
+public class Principal {
+    public static void main(String[] args) {
+        Animais cachorro = new Animais();
+
+        cachorro.nome = "cachorro";
+        cachorro.respostaCachorro();
+    }
+}
